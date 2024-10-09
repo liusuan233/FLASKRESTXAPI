97e3845258af72a42330be09b92d57d6d63ddbee
relax <3145849381@qq.com>
2024-10-09 12:15:23 +0800

    更新gitignore

--- app.py
+++ app.py
@@ -7,20 +7,15 @@ def create_app():
     # 初始化Flask应用
     app = Flask(__name__)# __main__
     # 配置数据库
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/nba_db'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nba_db.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+    # 关闭对模型修改的监控
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # 初始化数据库
     db.init_app(app)
-
     # 初始化API
     api = Api(app, version='1.0', title='NBA API', description='This is a NBA API')
+    # 注册路由
     api.add_namespace(ns_nba)
-
-    # # 注册路由
-    # api.add_resource(NBAResourcesDetail, "/NBA/<int:id>")
-    # api.add_resource(NBAResourcesList, "/NBA")
-
     return app
 
 
--- resources/NBAResources.py
+++ resources/NBAResources.py
@@ -3,8 +3,10 @@ from flask_restx import Resource,Namespace,fields
 from models.NBAModels import NBATeams
 from app import db
 
+# 创建命名空间ns
 ns = Namespace('nba', description='NBA related operations')
 
+# 创建模型
 player_model = ns.model("NBATeams", {
     'team_name': fields.String(required=True, description='Team name'),
     'founded_year': fields.Integer(required=True, description='Player founded year'),
@@ -21,13 +23,17 @@ class NBAResourcesDetail(Resource):
         This method returns the details of a team by its name.
         """
         try:
+            # 查询数据库
             NBAModels = db.session.query(NBATeams).filter_by(id=id).first()
+            # 返回查询到的球队信息
             return {"id": f"{NBAModels.id}",
                     "team_name": f"{NBAModels.team_name}",
                     "founded_year": f"{NBAModels.founded_year}",
                     "city": f"{NBAModels.city}",
                     "championships_won": f"{NBAModels.championships_won}",},200
+
         except Exception as e:
+            # 数据库查询失败
             return {"message": f"输入的球队名称有误，请检查后重试。{e}"}, 404
     @ns.doc(params={"id": "球队ID"})
     def delete(self,id):
@@ -36,9 +42,12 @@ class NBAResourcesDetail(Resource):
         This method deletes a team by its name.
         """
         try:
+            # 查询数据库
             NBATeamsInfo = NBATeams.query.filter(NBATeams.id == id).first()
+            # 删除球队信息
             db.session.delete(NBATeamsInfo)  # 删除查询到的记录
             db.session.commit()
+            # 返回删除成功信息
             return {"message": "球队删除成功！","球队ID":f"{id}"}, 200
         except Exception as e:
             return {"message": f"球队删除失败！{e}"}, 500
@@ -53,12 +62,16 @@ class NBAResourcesDetail(Resource):
         try:
             NBAteaminfo = request.json
             if NBAteaminfo:
+                # 查询数据库
                 NBATeamsInfo = NBATeams.query.filter(NBATeams.id == id).first()
+                # 更新球队信息
                 NBATeamsInfo.team_name = NBAteaminfo['team_name']
                 NBATeamsInfo.city = NBAteaminfo['city']
                 NBATeamsInfo.founded_year = NBAteaminfo['founded_year']
                 NBATeamsInfo.championships_won = NBAteaminfo['championships_won']
+                # 更新数据库
                 db.session.commit()
+                # 返回更新后的球队信息
                 NBAModels = NBATeamsInfo
                 return {"message": "球队更新成功！",
                         "id": f"{NBAModels.id}",
@@ -80,7 +93,9 @@ class NBAResourcesList(Resource):
         This method returns a list of all teams.
         """
         try:
+            # 查询数据库
             NBAModels = db.session.query(NBATeams).all()
+            # 返回查询到的所有球队信息
             teams_list = []
             for team in NBAModels:
                 teams_list.append({"id": f"{team.id}",
@@ -103,6 +118,7 @@ class NBAResourcesList(Resource):
         try:
             if db.session.query(NBATeams).filter_by(team_name=request.json["team_name"]).first():
                 return {"message": "该球队已存在！"}, 400
+            # 接收请求数据
             request_json = request.json
             if request_json:
                 NBATeamsInfo = NBATeams(team_name=request_json["team_name"],
